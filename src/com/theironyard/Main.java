d1a6c8518ed9b9e338a00e0d62ad6984ca475bc5
Paige Hetherington <herbmama@gmail.com>
2016-03-01 12:20:12 -0500

    Add database

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,10 +1,12 @@
 package com.theironyard;
 
+import com.sun.xml.internal.xsom.impl.util.SchemaTreeTraverser;
 import spark.ModelAndView;
 import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.SplittableRandom;
@@ -12,13 +14,82 @@ import java.util.StringJoiner;
 
 public class Main {
 
-    static HashMap<String, User> users = new HashMap<>();
-    static ArrayList<Message> messages = new ArrayList<>();
+//    static HashMap<String, User> users = new HashMap<>();
+//    static ArrayList<Message> messages = new ArrayList<>();
+
+    public static void createTables(Connection conn) throws SQLException {
+        Statement stmt = conn.createStatement();
+        stmt.execute("CREATE TABLE IF NOT EXISTS users (id IDENTITY, name VARCHAR, password VARCHAR)");
+        stmt.execute("CREATE TABLE IF NOT EXISTS messages (id IDENTITY, user_id INT, reply_id INT, text VARCHAR)");
 
 
-    public static void main(String[] args) {
-	    addTestUsers();
-        addTestMessages();
+    }
+
+    public static void insertUser (Connection conn, String name, String password) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO users VALUES (NULL, ?, ?)");
+        stmt.setString(1, name);
+        stmt.setString(2, password);
+        stmt.execute();
+    }
+
+    public static User selectUser (Connection conn, String name) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM users WHERE name = ?");
+        stmt.setString(1, name);
+        ResultSet results =stmt.executeQuery();
+        if (results.next()) { //if can find, returns user object
+            int id = results.getInt("id");
+            String password = results.getString("password");
+            return new User(id, name, password);
+        }
+        return null; //if can't find returns null
+    }
+
+    public static void insertMessage(Connection conn, int userId, int replyId, String text) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO messages VALUES (NULL, ?, ?, ?)");
+        stmt.setInt(1, userId);
+        stmt.setInt(2, replyId);
+        stmt.setString(3, text);
+        stmt.execute();
+
+    }
+
+    public static Message selectMessage(Connection conn, int id) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM messages INNER JOIN users ON messages.user_id = users.id WHERE messages.id = ?");
+        stmt.setInt(1, id);
+        ResultSet results = stmt.executeQuery();
+        if (results.next()) {
+            int replyId = results.getInt("messages.reply_id");
+            String name = results.getString("users.name");
+            String text = results.getString("messages.text");
+            return  new Message(id, replyId, name, text);
+        }
+        return null;
+    }
+
+    public static ArrayList<Message> selectReplies(Connection conn, int replyId) throws SQLException {
+        ArrayList<Message> messages = new ArrayList<>();
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM messages INNER JOIN users ON messages.user_id = users.id WHERE messages.reply_id = ?");
+        stmt.setInt(1, replyId);
+        ResultSet results = stmt.executeQuery();
+        while (results.next()) {
+            int id = results.getInt("messages.id");
+            String name = results.getString("users.name");
+            String text = results.getString("messages.text");
+            Message message = new Message(id, replyId, name, text);
+            messages.add(message);
+
+        }
+        return messages; //returns AL of message objects
+    }
+
+
+    public static void main(String[] args) throws SQLException {
+
+        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+        createTables(conn);
+
+//        addTestUsers();
+//        addTestMessages();
 
         Spark.init();
         Spark.get(
@@ -36,12 +107,12 @@ public class Main {
 
 
                     HashMap m = new HashMap();
-                    ArrayList<Message> threads = new ArrayList<>();
-                    for (Message message : messages) { //Filter
-                        if (message.replyId == replyIdNum) {
-                            threads.add(message);
-                        }
-                    }
+                    ArrayList<Message> threads = selectReplies(conn, replyIdNum); //replace with method
+//                    for (Message message : messages) { //Filter
+//                        if (message.replyId == replyIdNum) {
+//                            threads.add(message);
+//                        }
+                    //}
                     m.put("messages", threads);
                     m.put("userName", userName);
                     m.put("replyId", replyIdNum); //reply id ofpage itself so can access in mustache
@@ -58,10 +129,11 @@ public class Main {
                         throw new Exception("Login name not found.");
                     }
 
-                    User user = users.get(userName);
+                    User user = selectUser(conn, userName);
                     if (user == null) {
-                        user = new User(userName, "");
-                        users.put(userName, user);
+                        insertUser(conn, userName, "");
+//                        user = new User(userName, "");
+//                        users.put(userName, user);
                     }
                     Session session = request.session();
                     session.attribute("userName", userName);
@@ -95,8 +167,11 @@ public class Main {
                     }
                     int replyIdNum = Integer.valueOf(replyId);
 
-                    Message m = new Message(messages.size(), replyIdNum, userName, text); //message size creates id and gets next available number
-                    messages.add(m);
+                    User user = selectUser(conn, userName); //query user
+                    insertMessage(conn,user.id, replyIdNum, text ); //insert message with correct userid
+
+//                    Message m = new Message(messages.size(), replyIdNum, userName, text); //message size creates id and gets next available number
+//                    messages.add(m);
 
                     response.redirect(request.headers("Referer")); //referrer states what url it came from
                     return "";
@@ -105,17 +180,17 @@ public class Main {
         );
     }
 
-    static void addTestUsers() {
-        users.put("Alice", new User("Alice", ""));
-        users.put("Bob", new User("Bob", ""));
-        users.put("Charlie", new User("Charlie", ""));
-
-    }
-    static void addTestMessages() {
-        messages.add(new Message (0, -1, "Alice", "Hello world!"));
-        messages.add(new Message(1, -1, "Bob", "This is another thread!"));
-        messages.add(new Message(2, 0, "Charlie", "Cool thread, Alice."));
-        messages.add(new Message(3, 2, "Alice", "Thanks"));
-
-    }
+//    static void addTestUsers() {
+//        users.put("Alice", new User("Alice", ""));
+//        users.put("Bob", new User("Bob", ""));
+//        users.put("Charlie", new User("Charlie", ""));
+//
+//    }
+//    static void addTestMessages() {
+//        messages.add(new Message (0, -1, "Alice", "Hello world!"));
+//        messages.add(new Message(1, -1, "Bob", "This is another thread!"));
+//        messages.add(new Message(2, 0, "Charlie", "Cool thread, Alice."));
+//        messages.add(new Message(3, 2, "Alice", "Thanks"));
+//
+//    }
 }
--- src/com/theironyard/User.java
+++ src/com/theironyard/User.java
@@ -4,8 +4,15 @@ package com.theironyard;
  * Created by vajrayogini on 2/24/16.
  */
 public class User {
+    int id;
     String name;
     String password;
+
+    public User(int id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
 
     public User(String name, String password) {
         this.name = name;
